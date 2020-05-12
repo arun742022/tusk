4212721664faa71476a5dd236e5398d0c280658b
arun742022 <65158745+arun742022@users.noreply.github.com>
2020-05-12 14:10:17 +0530

    Added package and class Test.java

---
+++ Test Project/src/com/att/Test.java
@@ -0,0 +1,10 @@
+package com.att;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Pushing the code from local to Git Hub");
+	}
+
+}
